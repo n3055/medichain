4af1321b6255e66e9a69e48690b24bed67e158da
BharathSJ3026 <imbharaths24@gmail.com>
2025-03-20 02:49:26 +0530

    rent

--- medi_app/models.py
+++ medi_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 STATUS_CHOICES = [
     ("Pending", "Pending"),
@@ -10,20 +11,21 @@ STATUS_CHOICES = [
 
 class ProductImage(models.Model):
     pid = models.BigIntegerField(unique=True)  # Product ID from smart contract
-    owner = models.CharField(max_length=42)  # Owner's public address
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_product_images')  # Owner's public address
     image = models.ImageField(upload_to="product_images/")
 
 class RentalRequest(models.Model):
     pid = models.BigIntegerField()
-    owner = models.CharField(max_length=42)
-    renter = models.CharField(max_length=42)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_rentals')
+    renter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='rented_items')
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="Pending")
     created_at = models.DateTimeField(auto_now_add=True)
 
 class SubRentalRequest(models.Model):
     pid = models.BigIntegerField()
-    owner = models.CharField(max_length=42)
-    renter = models.CharField(max_length=42)  # Main renter
-    sub_renter = models.CharField(max_length=42)  # Sub-renter
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_subrentals')
+    renter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='rented_subrentals') # Main renter
+    sub_renter= models.ForeignKey(User, on_delete=models.CASCADE, related_name='sub_rented_items')   # Sub-renter
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="Pending")
     created_at = models.DateTimeField(auto_now_add=True)
+
